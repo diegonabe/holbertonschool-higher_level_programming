3ad318ec311f14d6830e8ae648bfee1132aefc1b
diego napan <diegonabe@hotmail.com>
2023-11-18 07:35:33 -0800

    Added scripts with class Square

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a class square"""
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size):
+        """Initialize the square
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
