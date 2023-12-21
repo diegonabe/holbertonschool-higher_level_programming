ee370b31fb3ff5d79740a156eeb35e2d94b1238f
diego napan <diegonabe@hotmail.com>
2023-12-21 07:28:11 -0800

    ADD creates a script that creates table unique ID

---
+++ SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,3 @@
+-- Write a script that creates the table unique ID
+-- make a table and let it have default and unique constants
+CREATE TABLE IF NOT EXISTS unique_id (id INT UNIQUE DEFAULT 1, name VARCHAR(256));
