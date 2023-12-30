e36c747acd854a8dd8d43bbdcc745828d806e9cc
diego napan <diegonabe@hotmail.com>
2023-12-30 15:53:41 -0800

    ADD program that prints the number of and the list of arguments

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
+    """Imprime el número y la lista de argumentos."""
     import sys
 
     count = len(sys.argv) - 1
     if count == 0:
-        print("0 arguments.")
+        print("0 argumentos.")
+    elif count == 1:
+        print("1 argumento:")
+    else:
+        print("{} argumentos:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
