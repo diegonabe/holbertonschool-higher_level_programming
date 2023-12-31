5cc3718b408bb59078c68e59e4ae80e3823eb1e1
diego napan <diegonabe@hotmail.com>
2023-12-31 07:46:55 -0800

    ADD script that list States from database using MySQLdb

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,43 +1,10 @@
 #!/usr/bin/python3
-#Lists all states from the database hbtn_0e_0_usa.
-import MySQLdb
+#Enlista los "Estados"de la base de datos
 import sys
-
-def list_states(username, password, db_name):
-    # Conectar a la base de datos
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=db_name
-    )
-
-    # Crear un objeto cursor
-    cursor = db.cursor()
-
-    # Ejecutar la consulta SQL
-    query = "SELECT * FROM states ORDER BY states.id;"
-    cursor.execute(query)
-
-    # Obtener y mostrar los resultados
-    results = cursor.fetchall()
-    for row in results:
-        print(row)
-
-    # Cerrar la conexión
-    cursor.close()
-    db.close()
+import MySQLdb
 
 if __name__ == "__main__":
-    # Verificar si se proporcionaron los argumentos necesarios
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <db_name>".format(sys.argv[0]))
-        sys.exit(1)
-
-    # Obtener los argumentos
-    username, password, db_name = sys.argv[1:]
-
-    # Llamar a la función para listar estados
-    list_states(username, password, db_name)
-
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states`")
+    [print(state) for state in c.fetchall()]
