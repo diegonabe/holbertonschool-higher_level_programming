11c5af68c8a1a9b4bef2ad6da20810f5d87cde67
diego napan <diegonabe@hotmail.com>
2023-12-31 09:09:04 -0800

    FIX imports in model_state.py

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python3
-# Define un modelo de Estado.
-# Hereda de SQLAlchemy Base y se vincula a la tabla MySQL states.
-
+"""
+Define un modelo de estado que contiene la definición de la clase
+ State y una instancia Base = declarative_base()
+"""
+from lib2to3.pytree import Base
+from sre_parse import State
+from unicodedata import name
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 class State(Base):
-    """Representa un estado para una base de datos MySQL.
-
-    __tablename__ (str): El nombre de la tabla MySQL para almacenar Estados.
-    id (sqlalchemy.Integer): El id del estado.
-    name (sqlalchemy.String): El nombre del estado.
+    """
+    Hereda de Base
+    Se vincula a la tabla MySQL states
+    Atributo de clase id que representa una columna
+     de un entero único generado automáticamente, no puede
+      ser nulo y es una clave primaria
+    Atributo de clase name que representa una columna
+     de una cadena con un máximo de 128 caracteres y
+      no puede ser nulo
     """
     __tablename__ = "states"
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
+
