3e653b332ccf149e50920aaa4e16ad57a3933766
diego napan <diegonabe@hotmail.com>
2023-12-30 16:04:36 -0800

    ADD function that prints the result of the sum of all args

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Imprime la suma de todos los argumentos"""
+    import sys
+
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
