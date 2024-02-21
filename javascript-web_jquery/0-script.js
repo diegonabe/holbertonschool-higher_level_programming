d76f4f4066ef66a2cd9dcaa528f7ffba01bd2c44
diego napan <130240874+diegonabe@users.noreply.github.com>
2024-02-21 18:14:26 -0500

    ADD script that changes to red color

---
+++ javascript-web_jquery/0-script.js
@@ -0,0 +1,5 @@
+// JS Script que cambia color del header
+document.addEventListener('DOMContentLoaded', () => {
+    const headerElement = document.querySelector('header')
+    headerElement.style.color = '#FF0000'
+  })
